80a5365cc5a838a08ef24193d5c0779b2258593d
JKtimosha <79272266+JKtimosha@users.noreply.github.com>
2021-08-12 09:54:53 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,8 @@ from os import system, name
 #Данного бота было лень писать, так как он состоит из сурса, моего друга Глютена
 #Мне известно, что самсунг отсюда коды спиздил
 #Поэтому, здесь я ничего не писал
-
+#А вообще Глютен мне скзаал, скопировать сюда код весь глютен башера
+# И написать дестроер краш бот
 intents = discord.Intents.default()
 intents.members = True
 client = commands.Bot(command_prefix='t!', intents=intents, help_command=None)
